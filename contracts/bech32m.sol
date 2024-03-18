f0a96108c35bc60bdc0a0b51f3de669a1bb1e5c3
Prajwol Gyawali <mail.prajwolgyawali@gmail.com>
2024-03-18 10:28:35 +0545

    docs: add references and comments

--- contracts/bech32m.sol
+++ contracts/bech32m.sol
@@ -1,10 +1,32 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
+/// @dev The verification of base32m encoding is based on the work at: https://github.com/bitcoinjs/bech32/blob/master/src/index.ts
+/// The code has been written to minimize gas on the EVM.
+
 contract bech32m {
 
-    // string constant ALPHABET = 'qpzry9x8gf2tvdw0s3jn54khce6mua7l';
-
+    /// @dev ALPHABET_INDEX is based on the ALPHABET_MAP: Ref: https://github.com/bitcoinjs/bech32/blob/master/src/index.ts#L2-L8
+    /// Since lookup on the mapping is expensive, this method is used to save gas cost
+    ///
+    ///   string constant ALPHABET = 'qpzry9x8gf2tvdw0s3jn54khce6mua7l';
+    ///
+    ///   const ALPHABET_MAP: { [key: string]: number } = {};
+    ///   for (let z = 0; z < ALPHABET.length; z++) {
+    ///     const x = ALPHABET.charAt(z);
+    ///     ALPHABET_MAP[x] = z;
+    ///   }
+    ///
+    ///   const ALPHABET_INDEX = []
+    ///   for (let i=48; i<=122; i++) {
+    ///     let char = String.fromCharCode(i);
+    ///     let z = ALPHABET_MAP[char];
+    ///     if (z === undefined) {
+    ///       ALPHABET_INDEX.push(255)
+    ///     } else {
+    ///       ALPHABET_INDEX.push(z)
+    ///     }
+    ///   }
     uint8[75] public ALPHABET_INDEX = [
         15, 255,  10,  17,  21,  20,  26,  30,   7,   5, 255, 255,
         255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
@@ -15,6 +37,8 @@ contract bech32m {
             6,   4,   2
     ];
 
+    /// @dev The original function used the neg (-) operator. Ref: https://github.com/bitcoinjs/bech32/blob/master/src/index.ts#L10-L20
+    /// Since the neg operator is not available for uint type, the logic is modified as follow
     function polymodStep(uint pre) internal pure returns (uint) {
         uint b = pre >> 25;
         return (
@@ -30,13 +54,20 @@ contract bech32m {
     function validateAleoAddr(string memory addr) external view returns ( bool ) {
         require(bytes(addr).length == 63, "Invalid Aleo address length");
 
+        // This value is the output of prefixChk("aleo"). Ref: https://github.com/bitcoinjs/bech32/blob/master/src/index.ts#L22
         uint chk = 393502710;
 
+        // 58 = 63 - len(aleo1)
+        // the validation is necessary only starting from the 5th byte of address since the first 5 bytes is encoded in `chk`
         for (uint i = 0; i < 58; i++) {
-            uint v = ALPHABET_INDEX[uint8(bytes(addr)[i+5]) - 48];
+            uint v = ALPHABET_INDEX[uint8(bytes(addr)[i+5]) - 48]; // 48 is the ASCII of 0 (zero)
             chk = polymodStep(chk) ^ v;
         }
+        
+        // Aleo uses bech32m encoding which has the given encoding constant
+        // Ref: https://github.com/bitcoinjs/bech32/blob/master/src/index.ts#L100
         uint ENCODING_CONST = 0x2bc830a3;
+
         require(chk == ENCODING_CONST, "Invalid checksum");
 
         return true;
